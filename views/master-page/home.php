946dd94ac567a96ec39706608072c9fc81654484
John Ivan Respende <JIRespende@gmail.com>
2023-01-12 11:45:24 +0800

    5th commit

--- views/master-page/home.php
+++ views/master-page/home.php
@@ -3,9 +3,9 @@
 
 <?php include '../layouts/head.php' ?>
 
-<?php
-if (!$_SESSION['user']) {
-    header("Location: login.php");
+<?php 
+if(!$_SESSION['user']) {
+    header("Location: login.php"); 
 }
 
 ?>
@@ -19,52 +19,60 @@ if (!$_SESSION['user']) {
 
     <div class="container">
 
-        <div class="table-grid table-grid--res">
-            <div class="box box--date-time">
-                <div class="box-wrapper">
-                    <div class="dateholder">
-                        <?php
-                        $monthNum  = date('m');
-                        $monthName = date('F', mktime(0, 0, 0, $monthNum, 10));
-                        ?>
-                        <h2><?php echo $monthName; ?></h2>
-                        <h2><?php echo date('d'); ?></h2>
-                        <h2><?php echo date('Y'); ?></h2>
-                        <h3><?php echo date('h:i:A') ?></h3>
+        <table class="table-grid">
+            <tr>
+                <td class="homebox" rowspan="2">
+                    <div class="box-wrapper">
+                        <div class="dateholder">
+                            <?php
+                             date_default_timezone_set("Asia/Singapore");
+                             $monthNum  = date('m');
+                             $monthName = date('F', mktime(0, 0, 0, $monthNum, 10));
+                             ?>
+                             <h2><?php echo $monthName; ?></h2>
+                             <h2><?php echo date('d'); ?></h2>
+                             <h2><?php echo date('Y'); ?></h2>
+                             <div class="pos__head__date">
+                               <span><?php echo date('l'); ?></span>
+                               <p class="pos__head__time"><?php echo date('h:i:s A'); ?></p>
+                             </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="box box--products">
-                <div class="box-wrapper">
-                    <div class="box">
-                        <h1 id="lbl_total_product"></h1>
-                        <h2>Total Products</h2>
+                </td>
+                <td class="homebox">
+                    <div class="box-wrapper">
+                        <div class="box">
+                            <h1 id="lbl_total_product"></h1>
+                            <h2>Total Products</h2>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="box box--sales">
-                <div class="box-wrapper">
-                    <div class="box">
-                        <h1 id="lbl_sales_today"></h1>
-                        <h2>Total Sales Today</h2>
+                </td>
+                <td class="homebox">
+                    <div class="box-wrapper">
+                        <div class="box">
+                            <h1 id="lbl_sales_today"></h1>
+                            <h2>Total Sales Today</h2>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="box box--report">
-                <div class="box-wrapper">
-                    <h5>Purchased Category per Month(6 Months)</h5>
-                    <canvas id="home_canvas"></canvas>
-                </div>
-            </div>
-        </div>
-
-
+                </td>
+            </tr>
+            <tr>
+                <td class="homebox" colspan="2">
+                    <div class="box-wrapper">
+                        <h5>Purchased Category per Month(6 Months)</h5>
+                        <canvas id="home_canvas"></canvas>
+                    </div>
+                </td>
+            </tr>
+        </table>
 
 
 
 
         <?php include '../layouts/scripts.php' ?>
 </body>
-<script src="../../libs/scripts/master-page/home.js"></script>
+<script src="../../libs/scripts/master-page/home.js" ></script>
+<script src="../../libs/scripts/pos/time.js"></script>
+
 
 </html>
